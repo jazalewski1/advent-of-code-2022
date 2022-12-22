2f05de41dd0cfc9871ec1d537fdda926773aaf92
Jakub Zalewski <jazalewski1@gmail.com>
2022-12-22 22:15:33 +0100

    Improve cmake add_test_module

--- cmake/AddTestModule.cmake
+++ cmake/AddTestModule.cmake
@@ -2,24 +2,29 @@ function(add_test_module)
 	cmake_parse_arguments(
 		"ARG"
 		""
-		"NAME;PATH"
-		"DEPENDS;INCLUDE_DIRS"
+		"NAME;SOURCES;OUTPUT_DIR"
+		"DEPENDS;INCLUDES"
 		${ARGN}
 	)
 
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH}/${ARG_OUTPUT_DIR})
+
 	add_executable(
 		${ARG_NAME}
-		${ARG_PATH}
+		${ARG_SOURCES}
 	)
 
 	target_link_libraries(
 		${ARG_NAME}
+		gtest
+		gtest_main
+		utility
 		${ARG_DEPENDS}
 	)
 
 	target_include_directories(
 		${ARG_NAME}
-		PRIVATE ${ARG_INCLUDE_DIRS}
+		PRIVATE ${ARG_INCLUDES}
 	)
 
 	add_test(NAME ${ARG_NAME} COMMAND ${ARG_NAME})
